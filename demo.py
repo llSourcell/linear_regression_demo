07b532f7c17d9da105dc4aa7ef8dd17e9d58624a
satishjasthi <satish27may@gmail.com>
2017-01-17 13:12:51 +0530

    Creat multipe features using sklearn.preprocessing.PolynomialFeatures and train Linear regression on it.

--- demo.py
+++ demo.py
@@ -1,9 +1,47 @@
-import pandas as pd
-from sklearn import linear_model
+import pandas as pd;import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn import linear_model
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import cross_val_predict
+from sklearn.cross_validation import cross_val_score
+from IPython.display import display
 #read data
-dataframe = pd.read_fwf('brain_body.txt')
+dataframe = pd.read_csv('challenge_dataset.txt',names = ['Brain','Body'])
+features = dataframe
+poly = PolynomialFeatures(2)
+features = poly.fit_transform(dataframe)
+features = pd.DataFrame(features,columns =['constant','Brain','Body','Brain**2','Brain*Body','Body**2'])
+target = features['Body']
+features = features.drop('Body',axis= 1);
+
+lr = LinearRegression()
+predicted = cross_val_predict(lr,features,target,cv = 5)
+scores = cross_val_score(lr,features,target,cv=5,scoring = 'r2')
+scores
+fig,ax = plt.subplots()
+ax.scatter(target,predicted)
+ax.plot([target.min(),target.max()],[target.min(),target.max()],'k--',lw=4)
+ax.set_xlabel("Features")
+ax.set_ylabel("Body weight")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+print np.size(dataframe,axis=0)
 x_values = dataframe[['Brain']]
 y_values = dataframe[['Body']]
 
@@ -14,4 +52,4 @@ body_reg.fit(x_values, y_values)
 #visualize results
 plt.scatter(x_values, y_values)
 plt.plot(x_values, body_reg.predict(x_values))
-plt.show()
+plt.show() '''
