9945905eaa802431e9af95969d7ef191d8f79ca3
satishjasthi <satish27may@gmail.com>
2017-01-17 14:30:01 +0530

    remove all code related to LR with multiple features

--- demo.py
+++ demo.py
@@ -2,13 +2,9 @@ import pandas as pd;import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import linear_model
-from sklearn.feature_selection import RFE
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import cross_val_predict
-from sklearn.cross_validation import cross_val_score
 from sklearn.metrics import mean_squared_error
-from IPython.display import display
+
 #read data
 dataframe = pd.read_csv('challenge_dataset.txt',names = ['Brain','Body'])
 
@@ -32,23 +28,3 @@ print "r-square score:",body_reg.score(x_values,y_values)
 print "slope:" ,body_reg.coef_
 print "intercept:", body_reg.intercept_
 print "MSE value:", mean_squared_error(y_values,pred)
-
-
-
-'''features = dataframe
-poly = PolynomialFeatures(2)
-features = poly.fit_transform(dataframe)
-features = pd.DataFrame(features,columns =['constant','Brain','Body','Brain**2','Brain*Body','Body**2'])
-target = features['Body']
-features = features.drop('Body',axis= 1);
-
-lr = LinearRegression()
-predicted = cross_val_predict(lr,features,target,cv = 5)
-scores = cross_val_score(lr,features,target,cv=5,scoring = 'r2')
-scores
-fig,ax = plt.subplots()
-ax.scatter(target,predicted)
-ax.plot([target.min(),target.max()],[target.min(),target.max()],'k--',lw=4)
-ax.set_xlabel("Features")
-ax.set_ylabel("Body weight")
-plt.show()'''
