3fba3cee55a8f8ecf78557f36bb7ccca585d790a
satishjasthi <satish27may@gmail.com>
2017-01-17 13:46:20 +0530

    Train LR with only 1 feature

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import pandas as pd;import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn import linear_model
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LinearRegression
@@ -9,7 +10,30 @@ from sklearn.cross_validation import cross_val_score
 from IPython.display import display
 #read data
 dataframe = pd.read_csv('challenge_dataset.txt',names = ['Brain','Body'])
-features = dataframe
+
+print np.size(dataframe,axis=0)
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+
+#train model on data
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+#visualize results
+plt.scatter(x_values, y_values)
+plt.plot(x_values, body_reg.predict(x_values))
+plt.xlabel('Brain_weights')
+plt.ylabel("Body_weights")
+plt.show()
+
+print "r-square score:",body_reg.score(x_values,y_values)
+print "slope:" ,body_reg.coef_
+print "intercept:", body_reg.intercept_
+
+
+
+
+'''features = dataframe
 poly = PolynomialFeatures(2)
 features = poly.fit_transform(dataframe)
 features = pd.DataFrame(features,columns =['constant','Brain','Body','Brain**2','Brain*Body','Body**2'])
@@ -25,31 +49,4 @@ ax.scatter(target,predicted)
 ax.plot([target.min(),target.max()],[target.min(),target.max()],'k--',lw=4)
 ax.set_xlabel("Features")
 ax.set_ylabel("Body weight")
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-print np.size(dataframe,axis=0)
-x_values = dataframe[['Brain']]
-y_values = dataframe[['Body']]
-
-#train model on data
-body_reg = linear_model.LinearRegression()
-body_reg.fit(x_values, y_values)
-
-#visualize results
-plt.scatter(x_values, y_values)
-plt.plot(x_values, body_reg.predict(x_values))
-plt.show() '''
+plt.show()'''
